4fae24437917dc9b3df0d10c012ee2700850d269
Holger Protzek <holger.protzek@springer.com>
2018-04-17 15:06:46 +0200

    added in cmd

--- out.js
+++ out.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+const readJSON = require("./helper").readJSON;
+
 (async () => {
-  process.stdout.write(`[]\n`);
+  const request = await readJSON(process.stdin);
+
+  var response = {};
+  response.version = request.version;
+
+  process.stdout.write(`${JSON.stringify(response, null, 2)}\n`);
 })();
